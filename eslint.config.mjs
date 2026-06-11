422ce6932eb954f3da76c6d829ce4fe21d4fedf3
ladoon2006-pixel <ladoon2006@gmail.com>
2026-06-11 19:29:14 +0330

    feat: add eslint config for TypeScript

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+{
+  "compilerOptions": {
+    "jsx": "react-jsx",
+    "jsxImportSource": "react"
+  },
+  "extends": "@eslint/js/configs/recommended",
+  "ignorePatterns": ["dist", "node_modules", "build"]
+}
